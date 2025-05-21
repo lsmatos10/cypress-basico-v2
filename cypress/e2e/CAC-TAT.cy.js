99d5b804c717c5176a8a74c509e5b5ecb8926fbb
Leonardo Matos <devleomatos@gmail.com>
2025-05-21 15:37:38 -0300

    Aula 2
    Comando customizado

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -7,6 +7,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
   })
   it('Preenche os campos obrigatórios', function(){
+    const longText = Cypress._.repeat('abcdefghijklmnoepqrstuvwxyz')
     cy.get('input[id="firstName"]')
       .should('be.visible')
       .type('Leonardo')
@@ -22,7 +23,125 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     cy.get('input[id="phone"]')
       .should('be.visible')
       .type('11999999')
+
+    cy.get('textarea[row="6"]')
+      .should('be.visible')
+      .type(longText, { delay: 0})
+
+    cy.get('button[type="submit"]')
+      .click()
+
+  
+    //cy.contains('strong', 'Mensagem enviada com sucesso.')
+    // .should('be.visible');
+
         
+  })
+
+  it('EXIBE MENSAGEM DE ERRO COM EMAIL INVALIDO', function(){
+    cy.get('input[id="firstName"]')
+      .should('be.visible')
+      .type('Leonardo')
+        
+    cy.get('input[id="lastName"]')
+      .should('be.visible')
+      .type('Matos')
+
+    cy.get('input[id="email"]')
+      .should('be.visible')
+      .type('lsmatos')
+          
+    cy.get('input[id="phone"]')
+      .should('be.visible')
+      .type('11999999')
+
+    cy.get('textarea[row="6"]')
+      .should('be.visible')
+      .type('Teste')
+
+    cy.get('button[type="submit"]')
+      .click()
+
+    cy.contains('strong', 'Valide os campos obrigatórios!').should('be.visible');
+
+
+  })
+    it('Valide o campo telefone, só aceite numero ', function(){
+  
+  
+          
+    cy.get('input[id="phone"]')
+    .type('abcde')
+      .should('have.value', '')
+
+
+    cy.get('button[type="submit"]')
+      .click()
+  })
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
+        
+    cy.get('input[id="firstName"]')
+      .should('be.visible')
+      .type('Leonardo')
+        
+    cy.get('input[id="lastName"]')
+      .should('be.visible')
+      .type('Matos')
+
+    cy.get('input[id="email"]')
+      .should('be.visible')
+      .type('lsmatos')
+          
+    //cy.get('input[id="phone"]')
+      //.should('be.visible')
+     // .type('11999999')
+
+    cy.get('textarea[row="6"]')
+      .should('be.visible')
+      .type('Teste')
+
+    cy.get('input[id="phone-checkbox"]')
+      .click()
+
+    cy.get('button[type="submit"]')
+      .click()
+
+    cy.contains('strong', 'Valide os campos obrigatórios!')
+      .should('be.visible');
+
+      })
+
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', function(){
+    cy.get('input[id="firstName"]')
+      .should('be.visible')
+      .type('Leonardo')
+      .clear()
+      .should('have.value', '')
+  })
+
+  it('envia o formuário com sucesso usando um comando customizado', () => {
+    
+    cy.fillMandatoryFieldsAndSubmit()//preencha os campos obrigatorios e envie
+
+    cy.get('button[type="submit"]')
+      .click()
+    cy.get('.success')
+      .should('be.visible')
+  })
+  it('Encontrando elemento através do contains', () => { //Sempre utilizar o contains para identificar um elemento que não tenha id
+    cy.fillMandatoryFieldsAndSubmit()
+    
+    cy.contains('button', 'Enviar').click()
+    cy.contains('strong', 'Mensagem enviada com sucesso.')
+      .should('be.visible');
+  })
+//Aula 3: 
+  it.only('Utilizar o Select', () => {
+    cy.get('select').select('Cursos').should('have.value', 'cursos') //Seleção pelo texto
+    //cy.get('select').select('youtube') //Seleção pelo value youtube
+    //cy.get('select').select(1) //Seleção pelo índice 1
+
+    //Se o select for de multiplas escolhasm deverá ser listado-os em um array
   })
 }) 
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,30 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (data = {
+    firstName: 'John',
+    lastName: 'Dowmn',
+    email:'jogndown@example.com',
+    tel: '1100000000',
+    text: 'texto',
+
+}) => {
+    cy.get('input[id="firstName"]')
+      .should('be.visible')
+      .type(data.firstName)
+        
+    cy.get('input[id="lastName"]')
+      .should('be.visible')
+      .type(data.lastName)
+
+    cy.get('input[id="email"]')
+      .should('be.visible')
+      .type(data.email)
+          
+    cy.get('input[id="phone"]')
+      .should('be.visible')
+      .type(data.tel)
+
+    cy.get('textarea[row="6"]')
+      .should('be.visible')
+      .type(data.text)
+
+})
