6da8824e7d802604d2cc9f48ec65855f7c4c7ddb
Leonardo Matos <devleomatos@gmail.com>
2025-06-10 18:04:24 -0300

    Pagina de forma independente
    Visitando e validando outra aba

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -203,5 +203,19 @@ describe('Central de Atendimento ao Cliente TAT', function() {
           expect(input[0].files[0].name).to.equal('example.json')
       })
   })
+  // Aula 8: Lidando com links que abrem em outra aba do navegador
+  it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique ', () => {
+    cy.contains('a', 'Política de Privacidade')
+      .should('have.attr', 'href', 'privacy.html')
+      .and('have.attr', 'target', '_blank')
+  })
+  it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+    cy.contains('a', 'Política de Privacidade')
+      .invoke('removeAttr', 'target')
+      .click()
+
+      cy.contains('h1', 'CAC TAT - Política de privacidade')
+        .should('be.visible')
+  })
 }) 
 
